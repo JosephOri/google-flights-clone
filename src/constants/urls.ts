03264134b7b42ccc1c0f0756b1a406db74f75609
Ori <ori100no@gmail.com>
2025-02-03 16:56:08 +0200

    added urls constants

---
+++ src/constants/urls.ts
@@ -0,0 +1,3 @@
+export const rapidApiUrl = import.meta.env.VITE_RAPID_URL
+export const searchFlightsUrl = `${rapidApiUrl}/searchFlights`
+export const searchAirportUrl = `${rapidApiUrl}/searchAirport`
