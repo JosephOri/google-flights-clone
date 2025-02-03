d5a9a7d93b983c83e21c34ff68f3413dcf6df7ca
Ori <ori100no@gmail.com>
2025-02-03 17:54:11 +0200

    updated urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,3 @@
-export const rapidApiUrl = import.meta.env.VITE_RAPID_URL
-export const searchFlightsUrl = `${rapidApiUrl}/searchFlights`
-export const searchAirportUrl = `${rapidApiUrl}/searchAirport`
+export const rapidApiUrl = import.meta.env.VITE_RAPID_URL as string;
+export const searchFlightsUrl = `${rapidApiUrl}/searchFlights`;
+export const searchAirportUrl = `${rapidApiUrl}/searchAirport`;
