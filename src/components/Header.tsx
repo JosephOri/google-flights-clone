9f3fe9c3737d104c0c95bc9a10350f55c4859816
Ori <ori100no@gmail.com>
2025-02-04 16:35:06 +0200

    removed unedded component

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useThemeMode } from '../context/hooks/useThemeMode';
-import { Button } from '@mui/material';
-const Header = () => {
-  const { setIsDarkMode } = useThemeMode();
-  return <Button onClick={() => setIsDarkMode((prev) => !prev)}>change theme</Button>;
-};
-
-export default Header;
