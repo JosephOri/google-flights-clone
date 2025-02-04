f857cd8ec4b669fa26e798ab7e0b6e1983adabc7
Ori <ori100no@gmail.com>
2025-02-04 14:59:45 +0200

    changed filename

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const rapidApiUrl = import.meta.env.VITE_RAPID_URL as string;
+export const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY as string;
+export const rapidApiHost = import.meta.env.VITE_RAPID_HOST as string;
 export const searchFlightsUrl = `${rapidApiUrl}/searchFlights`;
 export const searchAirportUrl = `${rapidApiUrl}/searchAirport`;
